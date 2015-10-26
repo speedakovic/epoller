5d5312a0c6838573d554ad1f52d83bb3ad947d7d
Tomas Tvaroha <tomas.tvaroha@e4t.cz>
2015-10-26 15:17:49 +0100

    Add version headers

---
+++ src/epoller/version.h
@@ -0,0 +1,16 @@
+/**
+ *
+ * @file    version.h
+ * @author  speedak
+ * @brief   Version header.
+ *
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define EPOLLER_VERSION_MAJOR 1
+#define EPOLLER_VERSION_MINOR 1
+
+#endif /* VERSION_H */
+
---
+++ src/linbuff/version.h
@@ -0,0 +1,16 @@
+/**
+ *
+ * @file    version.h
+ * @author  speedak
+ * @brief   Version header.
+ *
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#define LINBUFF_VERSION_MAJOR 1
+#define LINBUFF_VERSION_MINOR 3
+
+#endif /* VERSION_H */
+
