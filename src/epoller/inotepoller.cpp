ff08a5af8a16e656a5f2f61f7f0a911968316a1a
Tomas Tvaroha <tomas.tvaroha@e4t.cz>
2015-11-20 11:47:00 +0100

    Add inotify epoller

---
+++ src/epoller/inotepoller.cpp
@@ -0,0 +1,135 @@
+#include <epoller/inotepoller.h>
+#include <unistd.h>
+#include <climits>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+#define DBG_PREFIX "inotepoller: "
+
+bool inotepoller::init()
+{
+	int ret;
+
+	// check file descriptor
+	if (fd != -1) {
+		std::cerr << DBG_PREFIX"already initialized" << std::endl;
+		goto unwind;
+	}
+
+	// create inotify file descriptor
+	fd = inotify_init();
+	if (fd == -1) {
+		perror(DBG_PREFIX"file descriptor creation failed");
+		goto unwind;
+	}
+
+	// add inotify file descriptor to epoller
+	memset(&event, 0, sizeof event);
+	event.data.ptr = this;
+	event.events = EPOLLIN;
+	ret = epoll_ctl(epoller->fd, EPOLL_CTL_ADD, fd, &event);
+	if (ret == -1) {
+		perror(DBG_PREFIX"adding file descriptor to epoller failed");
+		goto unwind_fd;
+	}
+
+	return true;
+
+unwind_fd:
+	close(fd);
+	fd = -1;
+
+unwind:
+	return false;
+}
+
+void inotepoller::cleanup()
+{
+	// check file descriptor
+	if (fd == -1)
+		return; // already cleaned-up
+
+	// remove inotify file descriptor from epoller
+	int ret = epoll_ctl(epoller->fd, EPOLL_CTL_DEL, fd, NULL);
+	if (ret == -1)
+		perror(DBG_PREFIX"removing file descriptor from epoller failed");
+
+	// close and invalidate inotify file descriptor
+	close(fd);
+	fd = -1;
+}
+
+int inotepoller::add_watch(const std::string &pathname, uint32_t mask)
+{
+	int wd = inotify_add_watch(fd, pathname.c_str(), mask);
+	if (wd == -1)
+		perror(DBG_PREFIX"adding watch failed");
+
+	return wd;
+}
+
+bool inotepoller::rm_watch(int wd)
+{
+	int ret = inotify_rm_watch(fd, wd);
+	if (ret == -1) {
+		perror(DBG_PREFIX"removing watch failed");
+		return false;
+	}
+
+	return true;
+}
+
+int inotepoller::handler(struct epoller *epoller, struct epoll_event *revent)
+{
+	int ret;
+	uint8_t buff[sizeof(struct inotify_event) + NAME_MAX + 1];
+	struct inotify_event *event = (struct inotify_event *)buff;
+
+	if (revent->events & EPOLLIN) {
+		revent->events &= ~EPOLLIN;
+
+		ret = read(fd, event, sizeof buff);
+		if (ret == -1) {
+			perror(DBG_PREFIX"reading from file descriptor failed");
+			return -1;
+
+		} else if (ret == 0) {
+			perror(DBG_PREFIX"no data read from file descriptor");
+			return -1;
+
+		} else {
+			if (ret < (int)sizeof(struct inotify_event)) {
+				perror(DBG_PREFIX"mismatched data read from file descriptor");
+				return -1;
+			}
+
+			return inothandler(event);
+		}
+	}
+
+	if (revent->events & EPOLLHUP) {
+		revent->events &= ~EPOLLHUP;
+		std::cerr << DBG_PREFIX"unexpected EPOLLHUP on file descriptor" << std::endl;
+		return -1;
+	}
+
+	if (revent->events & EPOLLERR) {
+		revent->events &= ~EPOLLERR;
+		std::cerr << DBG_PREFIX"unexpected EPOLLERR on file descriptor" << std::endl;
+		return -1;
+	}
+
+	if (revent->events) {
+		std::cerr << DBG_PREFIX"unexpected unknown event on file descriptor, events = " << revent->events << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
+
+int inotepoller::inothandler(struct inotify_event *event)
+{
+	return _inothandler ? _inothandler(this, event) : 0;
+}
+
---
+++ src/epoller/inotepoller.h
@@ -0,0 +1,63 @@
+/// @file   inotepoller.h
+/// @author speedak
+/// @brief  Inotify file descriptor wrapper.
+
+#ifndef INOTEPOLLER_H
+#define INOTEPOLLER_H
+
+#include <epoller/epoller.h>
+#include <sys/inotify.h>
+#include <stdint.h>
+#include <string>
+
+/// @brief Inotify epoller.
+struct inotepoller : public epoller_event
+{
+	int                fd;      ///< inotify file descriptor
+	struct epoller    *epoller; ///< parent epoller
+	struct epoll_event event;   ///< epoll event
+
+	/// @brief Handler for inotify event.
+	/// @see #inothandler
+	/// @param inotepoller inotify epoller within that the event occurred
+	int (*_inothandler) (struct inotepoller *inotepoller, struct inotify_event *event);
+
+	/// @brief Constructor.
+	/// @param epoller parent epoller
+	inotepoller(struct epoller *epoller) : fd(-1), epoller(epoller), event(), _inothandler(0) {}
+
+	/// @brief Destructor.
+	virtual ~inotepoller() {cleanup();}
+
+	/// @brief Initializes the inotify epoller.
+	/// @return @c true if initialization was successful, otherwise @c false
+	virtual bool init();
+
+	/// @brief Cleanups the inotify epoller.
+	virtual void cleanup();
+
+	/// @brief Adds watch.
+	/// @param pathname watched pathname
+	/// @param mask mask with monitoring events (IN_OPEN, IN_MODIFY, IN_CLOSE_WRITE, ...)
+	/// @return nonnegative watch descriptor if adding was successful, otherwise -1
+	int add_watch(const std::string &pathname, uint32_t mask);
+
+	/// @brief Removes watch.
+	/// @param wd watch descriptor to be removed
+	/// @return @c true if removing was successful, otherwise @c false
+	bool rm_watch(int wd);
+
+	/// @copydoc epoller_event::handler
+	virtual int handler(struct epoller *epoller, struct epoll_event *revent);
+
+	/// @brief Called when event occurs.
+	///
+	/// Default implementation calls #_inothandler if not null, otherwise returns 0.
+	///
+	/// @param event structure with information about the occurred event
+	/// @return zero for loop continuation, positive for normal loop exit, negative for loop exit with error
+	virtual int inothandler(struct inotify_event *event);
+};
+
+#endif // INOTEPOLLER_H
+
